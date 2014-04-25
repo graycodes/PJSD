ae5f7e4609b4019464d2aae96eac8106f62aed72
Gray MacGregor <gmacg.uk@gmail.com>
2014-04-25 18:41:34 +0100

    Improved interface.

--- JSDoc.pm
+++ JSDoc.pm
@@ -6,23 +6,21 @@ use warnings;
 
 use FileReader;
 
-print "Hello, this is JSDoc. \n";
-
 sub init {
-    my $input_file;
+    print "Hello, this is JSDoc. \n";
+    print "File to read: ";
 
+    my $input_file;
     $input_file = <STDIN>;
     chomp( $input_file );
     return unless $input_file;
-    print "Input file: $input_file\n";
 
     my $file = FileReader->new( file_name => $input_file );
     return unless $file;
 
-    $file->print_doc('ararararawr');
+    $file->print_doc();
 
     print "Thank you, come again. \n";
-
 }
 
 init();
