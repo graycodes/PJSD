7b9b1b40c84d33f20fb558cca10c8f03d4e67a56
Gray MacGregor <gmacg.uk@gmail.com>
2014-04-26 17:17:25 +0100

    Refactored methods into the right classes.

--- FileReader.pm
+++ FileReader.pm
@@ -10,86 +10,15 @@ sub new {
     return $self;
 }
 
-sub print_doc {
-    my $self = shift;
-    return unless $self->{file_name};
-
-    my @file_contents = read_file($self->{file_name});
-    return unless @file_contents;
-
-    my @docs = find_docs( @file_contents );
-    print "\n*** PRINTING DOCS ***\n";
-
-    @docs = parse_docs( @docs );
-    foreach ( @docs ) {
-        print $_, " -------------------------------------------------- \n";
-    }
-}
-
 sub read_file {
-    my $file_name = shift;
-    return unless $file_name;
+    my $self = shift;
+    my $file_name = $self->{file_name};
+    return unless $self->{file_name};
 
     open(my $file_handle, '<', $file_name)
         or die "Unable to open file: $!...\n";
 
-    return <$file_handle>;
-}
-
-sub find_docs {
-    my @contents = @_;
-    my @docs;
-    my $doc = '';
-    my $in_doc = 0;
-    my $push = 0;
-    my $start = 0;
-    my $end = 0;
-    my $next_line = 0;
-
-    foreach (@contents) {
-        $start = _is_doc_start( $_ );
-        $end   = _is_doc_end( $_ );
-
-        if ( !$start && !$end && !$in_doc && !$next_line ) {
-            next;
-        }
-
-        $in_doc = 1 if $start;
-        $in_doc = 0 if $end;
-
-        $doc .= $_;
-
-        if ( $next_line ) {
-            push( @docs, $doc );
-            $doc = '';
-            $next_line = 0;
-        }
-
-        next unless $end;
-        $next_line = 1;
-
-    }
-
-    return @docs;
-}
-
-sub parse_docs {
-    my @docs = @_;
-    my @output;
-
-    @output = map { (my $s = $_) =~ s/^ *(\*\/|\/\*\*|\*)//gm; $s } @docs;
-
-    return @output;
-}
-
-sub _is_doc_start {
-    my $line = shift;
-    return $line =~ /\/\*\*/;
-}
-
-sub _is_doc_end {
-    my $line = shift;
-    return $line =~ /\*\//;
+    return <$file_handle>;# file contents
 }
 
 1;
--- JSDoc.pm
+++ JSDoc.pm
@@ -10,20 +10,89 @@ sub init {
     print "Hello, this is JSDoc. \n";
     print "File to read: ";
 
-    my $input_file;
-    $input_file = <STDIN>;
+    my $input_file = <STDIN>;
     chomp( $input_file );
     #DEBUG - TODO
-    $input_file = 'maze.js';
+    $input_file = 'maze.js' unless $input_file;
+    #return unless $input_file;
     #DEBUG - TODO
-    return unless $input_file;
 
-    my $file = FileReader->new( file_name => $input_file );
-    return unless $file;
+    my $file_reader = FileReader->new( file_name => $input_file );
+    return unless $file_reader;
 
-    $file->print_doc();
+    my @file_contents = $file_reader->read_file();
+    print_doc( @file_contents );
 
     print "Thank you, come again. \n";
+}
+
+sub print_doc {
+    my @file_contents = @_;
+    return unless @file_contents;
+
+    my @docs = find_docs( @file_contents );
+    print "\n*** PRINTING DOCS ***\n";
+
+    @docs = parse_docs( @docs );
+    foreach ( @docs ) {
+        print $_, " -------------------------------------------------- \n";
+    }
+}
+
+sub find_docs {
+    my @contents = @_;
+    my @docs;
+    my $doc = '';
+    my $in_doc = 0;
+    my $push = 0;
+    my $start = 0;
+    my $end = 0;
+    my $next_line = 0;
+
+    foreach (@contents) {
+        $start = _is_doc_start( $_ );
+        $end   = _is_doc_end( $_ );
+
+        if ( !$start && !$end && !$in_doc && !$next_line ) {
+            next;
+        }
+
+        $in_doc = 1 if $start;
+        $in_doc = 0 if $end;
+
+        $doc .= $_;
+
+        if ( $next_line ) {
+            push( @docs, $doc );
+            $doc = '';
+            $next_line = 0;
+        }
+
+        next unless $end;
+        $next_line = 1;
+
+    }
+
+    return @docs;
+}
+
+sub parse_docs {
+    my @docs = @_;
+    my @output;
+
+    @output = map { (my $s = $_) =~ s/^ *(\*\/|\/\*\*|\*)//gm; $s } @docs;
+
+    return @output;
+}
+
+sub _is_doc_start {
+    my $line = shift;
+    return $line =~ /\/\*\*/;
+}
+
+sub _is_doc_end {
+    my $line = shift;
+    return $line =~ /\*\//;
 }
 
 init();
